c83f7cd6440e4a586bb9299149c826733e25ddb4
Wellington Ribeiro <devwellington@gmail.com>
2014-11-13 23:32:26 -0200

    Validate if is empty

--- src/Ribeiro/Validator/ValidatorName.php
+++ src/Ribeiro/Validator/ValidatorName.php
@@ -6,7 +6,7 @@ class ValidatorName extends AbstractValidator
 {
     public function validate()
     {
-        return (!is_string($this->value))
+        return (!is_string($this->value) || ($this->value == ''))
             ? "<li>O campo [{$this->name}] nao foi preenchido ou e invalido.</li>"
             : true
         ;
